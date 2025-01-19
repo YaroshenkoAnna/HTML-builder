03c9cf674ad179ec26ba4db6e5ece8be19cc6a52
Anna <yaroshenkoannette@gmail.com>
2025-01-19 23:12:21 +0100

    feat: add replacement of templates in task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,6 +1,13 @@
-const { join, extname } = require('path');
-const { copyFile, mkdir, rm, readdir, writeFile } = require('fs/promises');
-const { createReadStream } = require('fs');
+const { join, extname, basename } = require('path');
+const {
+  copyFile,
+  mkdir,
+  rm,
+  readdir,
+  readFile,
+  writeFile,
+} = require('fs/promises');
+const { createReadStream, createWriteStream } = require('fs');
 
 (async function (dest, srcCopy) {
   try {
@@ -13,6 +20,7 @@ const { createReadStream } = require('fs');
 
     compile('styles', destPath);
     copy(srcCopyPath, destCopyPath);
+    replace('components', 'template.html', destPath);
   } catch (err) {
     console.error(err);
   }
@@ -35,25 +43,49 @@ async function copy(src, dest) {
 }
 
 async function compile(srcDir, destDir) {
-  try {
-    const srcDirPath = join(__dirname, srcDir);
-    const destFilePath = join(destDir, 'style.css');
+  const srcDirPath = join(__dirname, srcDir);
+  const destFilePath = join(destDir, 'style.css');
 
-    await writeFile(destFilePath, '');
-    const files = await readdir(srcDirPath, { withFileTypes: true });
-    for (const file of files) {
-      if (file.isFile() && extname(file.name) === '.css') {
-        const srcFilePath = join(srcDirPath, file.name);
-        const stream = createReadStream(srcFilePath, 'utf-8');
-        let fileData = '';
+  await writeFile(destFilePath, '');
+  const files = await readdir(srcDirPath, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isFile() && extname(file.name) === '.css') {
+      const srcFilePath = join(srcDirPath, file.name);
+      const stream = createReadStream(srcFilePath, 'utf-8');
+      let fileData = '';
 
-        for await (const chunk of stream) {
-          fileData += chunk;
-        }
-        await writeFile(destFilePath, fileData + '\n', { flag: 'a' });
+      for await (const chunk of stream) {
+        fileData += chunk;
       }
+      await writeFile(destFilePath, fileData + '\n', { flag: 'a' });
     }
-  } catch (err) {
-    console.error(err);
   }
+}
+
+async function replace(srcDir, template, destDir) {
+  const srcDirPath = join(__dirname, srcDir);
+  const srcTemplatePath = join(__dirname, template);
+  const destFilePath = join(destDir, 'index.html');
+
+  const templateData = await readFile(srcTemplatePath, 'utf-8');
+
+  const fileExt = '.html';
+  const files = await readdir(srcDirPath, { withFileTypes: true });
+  const htmlReplacements = {};
+  for (const file of files) {
+    if (file.isFile() && extname(file.name) === fileExt) {
+      const fileName = basename(file.name, fileExt);
+      const filePath = join(srcDirPath, file.name);
+      const fileData = await readFile(filePath, 'utf-8');
+      htmlReplacements[fileName] = fileData;
+    }
+  }
+  let resultHtml = templateData;
+  for (const [key, value] of Object.entries(htmlReplacements)) {
+    const placeholder = `{{${key}}}`;
+    resultHtml = resultHtml.replace(new RegExp(placeholder, 'g'), value);
+  }
+  const writeStream = createWriteStream(destFilePath, 'utf-8');
+  writeStream.write(resultHtml);
+  writeStream.end();
 }
