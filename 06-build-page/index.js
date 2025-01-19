4d2b3031dca7b6d3a775b6dbe8933b48b34547ba
Anna <yaroshenkoannette@gmail.com>
2025-01-19 21:41:36 +0100

    feat: add compilation of styles in 6 Task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,15 +1,18 @@
-const { join } = require('path');
-const { copyFile, mkdir, rm, readdir } = require('fs/promises');
+const { join, extname } = require('path');
+const { copyFile, mkdir, rm, readdir, writeFile } = require('fs/promises');
+const { createReadStream } = require('fs');
 
 (async function (dest, srcCopy) {
   try {
     const srcCopyPath = join(__dirname, srcCopy);
-    const destPath = join(__dirname, dest, srcCopy);
+    const destPath = join(__dirname, dest);
+    const destCopyPath = join(destPath, srcCopy);
 
-    await rm(destPath, { recursive: true, force: true });
-    await mkdir(destPath, { recursive: true });
+    await rm(destCopyPath, { recursive: true, force: true });
+    await mkdir(destCopyPath, { recursive: true });
 
-    await copy(srcCopyPath, destPath);
+    compile('styles', destPath);
+    copy(srcCopyPath, destCopyPath);
   } catch (err) {
     console.error(err);
   }
@@ -28,5 +31,29 @@ async function copy(src, dest) {
       await mkdir(destPath, { recursive: true });
       await copy(srcPath, destPath);
     }
+  }
+}
+
+async function compile(srcDir, destDir) {
+  try {
+    const srcDirPath = join(__dirname, srcDir);
+    const destFilePath = join(destDir, 'style.css');
+
+    await writeFile(destFilePath, '');
+    const files = await readdir(srcDirPath, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile() && extname(file.name) === '.css') {
+        const srcFilePath = join(srcDirPath, file.name);
+        const stream = createReadStream(srcFilePath, 'utf-8');
+        let fileData = '';
+
+        for await (const chunk of stream) {
+          fileData += chunk;
+        }
+        await writeFile(destFilePath, fileData + '\n', { flag: 'a' });
+      }
+    }
+  } catch (err) {
+    console.error(err);
   }
 }
